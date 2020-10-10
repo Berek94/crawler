90c86b5fc2f6cf19f6d83edc52ac0785c31a7da9
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2020-10-10 23:47:01 +0300

    add deploy action

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#copy build
-tar czf build.tar.gz build
-scp build.tar.gz deployer@82.148.19.60:~
-rm -rf build.tar.gz
+#copy files
+tar czf build-crawler.tar.gz src node_modules .env
+scp build-crawler.tar.gz deployer@82.148.19.60:~
+rm -rf build-crawler.tar.gz
 
-#running
+#sending and run
 ssh deployer@82.148.19.60 <<'ENDSSH'
-rm -rf browser-worker
-mkdir browser-worker
-tar xf build.tar.gz -C browser-worker
-rm -rf build.tar.gz
+rm -rf crawler
+mkdir crawler
+tar xf build-crawler.tar.gz -C crawler
+rm -rf build-crawler.tar.gz
+cd crawler
+npm i puppeteer
+pm2 start crawler
 ENDSSH
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 const startSever = require("./server");
 
 startSever({ port: process.env.PORT });
